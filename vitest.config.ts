88e279b1fe16e7524b77930dc52cd77f3cc3fbaa
myzkey <miyazakiyasunari@gmail.com>
2025-11-28 06:05:45 +0900

    fix: test

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     alias: {
       '@lark-kit/core': new URL('./packages/core/src/index.ts', import.meta.url).pathname,
       '@lark-kit/shared': new URL('./packages/shared/src/index.ts', import.meta.url).pathname,
-      '@lark-kit/base': new URL('./packages/base/src/index.ts', import.meta.url).pathname,
+      '@lark-kit/bitable': new URL('./packages/bitable/src/index.ts', import.meta.url).pathname,
       '@lark-kit/chat': new URL('./packages/chat/src/index.ts', import.meta.url).pathname,
       'lark-kit': new URL('./packages/lark-kit/src/index.ts', import.meta.url).pathname,
     },
